3cea386d929c6fc213ca79278c9c84d1b4c19037
Joe Czubiak <jcz530@uw.edu>
2013-03-09 00:20:16 -0800

    added CreateAccount.class and HearingTest.class as well as some other xml files

--- Hearing Test/src/com/capstone/hearingtest/Main.java
+++ Hearing Test/src/com/capstone/hearingtest/Main.java
@@ -18,7 +18,14 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
@@ -37,64 +44,26 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class Main extends Activity{
     private static final String KEY_121 = "http://webhost.ischool.uw.edu/~jcz530/capstone/android/create-user.php";
+    private SharedPreferences user_info;
+    Context ctx = this;
 
-    SeekBar seekbar;
-    double num;
-    TextView value;
-
-	//test again and again
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.helloworld);
-
-		value = (TextView) findViewById(R.id.textview);
-		seekbar = (SeekBar) findViewById(R.id.seekbar);
-		num = 0;
-		 
-//		PlayFrequency.genTone(3, 1200);
-//		PlayFrequency.playSound();
+		setContentView(R.layout.main);
 		
-//		Plays the sound every time the button is pressed.
-		Button b = (Button) findViewById(R.id.BTN_play);
+	
+		Button b = (Button) findViewById(R.id.btn_getstarted);
 		b.setOnClickListener(new OnClickListener(){
+
 			public void onClick(View arg0) {
-				Log.d("Main", "button pressed");//this shows up in the LogCat. Helpful for debugging.
-				int input = 0;
-				EditText et = (EditText) findViewById(R.id.ET_frequency);
-				String s = et.getText().toString();
-				input = Integer.parseInt(s);//retrieve the input and parse as INT
-				Log.d("Main", "freq"+input);//this shows up in the LogCat. Helpful for debugging.
-
-				PlayFrequency.genTone(3, input);
-				PlayFrequency.playSound((float)num, (float)num);
+				Intent intent = new Intent(ctx, CreateAccount.class);
+				ctx.startActivity(intent);
 			}
 		});
-//		this is an example of how to push a test result to our db. 
-//		new PushToDB().execute("testresult","1", "880", "4");//this pushed test result to the db.
-		
-		seekbar.setOnSeekBarChangeListener( new OnSeekBarChangeListener()
-		{
-			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
-			{
-				num = (float)progress / 1000;
-				value.setText("Volume is at" + progress + "%");
-			}
-
-		    public void onStartTrackingTouch(SeekBar seekBar)
-		    {
-		    // TODO Auto-generated method stub
-		    }
-
-		    public void onStopTrackingTouch(SeekBar seekBar)
-		    {
-		    // TODO Auto-generated method stub
-		    }
-		});
-		
 	}
 
-    @Override
+	@Override
     protected void onResume() {
         super.onResume();
     }
