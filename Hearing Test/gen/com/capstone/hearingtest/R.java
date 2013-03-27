82f365d8a38e3ab232059233110d6e2dfd9a2775
Joe Czubiak <jcz530@uw.edu>
2013-03-27 14:00:55 -0700

    added AudioFxDemo.class to get started with audio manipulation

--- Hearing Test/gen/com/capstone/hearingtest/R.java
+++ Hearing Test/gen/com/capstone/hearingtest/R.java
@@ -9,8 +9,8 @@ package com.capstone.hearingtest;
 
 public final class R {
     public static final class array {
-        public static final int frequencies=0x7f040001;
-        public static final int gender=0x7f040000;
+        public static final int frequencies=0x7f050001;
+        public static final int gender=0x7f050000;
     }
     public static final class attr {
     }
@@ -18,21 +18,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int BTN_play=0x7f070007;
-        public static final int BTN_submit=0x7f07000a;
-        public static final int ET_frequency=0x7f070008;
-        public static final int btn_create_account=0x7f070006;
-        public static final int btn_getstarted=0x7f07000c;
-        public static final int seekbar=0x7f070009;
-        public static final int spinner_age=0x7f070005;
-        public static final int spinner_gender=0x7f070003;
-        public static final int textView1=0x7f070000;
-        public static final int textView2=0x7f070002;
-        public static final int textView3=0x7f070004;
-        public static final int textview=0x7f07000b;
-        public static final int toggleButton1=0x7f07000d;
-        public static final int tv_account=0x7f070001;
-        public static final int tv_freq=0x7f07000e;
+        public static final int BTN_play=0x7f080007;
+        public static final int BTN_submit=0x7f08000a;
+        public static final int ET_frequency=0x7f080008;
+        public static final int btn_create_account=0x7f080006;
+        public static final int btn_getstarted=0x7f08000c;
+        public static final int seekbar=0x7f080009;
+        public static final int spinner_age=0x7f080005;
+        public static final int spinner_gender=0x7f080003;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080002;
+        public static final int textView3=0x7f080004;
+        public static final int textview=0x7f08000b;
+        public static final int toggleButton1=0x7f08000d;
+        public static final int tv_account=0x7f080001;
+        public static final int tv_freq=0x7f08000e;
     }
     public static final class layout {
         public static final int create_account=0x7f030000;
@@ -42,11 +42,14 @@ public final class R {
         public static final int test_tone=0x7f030004;
         public static final int welcome=0x7f030005;
     }
+    public static final class raw {
+        public static final int test_cbr=0x7f040000;
+    }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int welcome=0x7f050001;
+        public static final int app_name=0x7f060000;
+        public static final int welcome=0x7f060001;
     }
     public static final class style {
-        public static final int ThemeLight=0x7f060000;
+        public static final int ThemeLight=0x7f070000;
     }
 }
