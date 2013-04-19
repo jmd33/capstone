7bf7e13814bf2b413a7a05539afaa15dd5896833
Joe Czubiak <jcz530@uw.edu>
2013-04-18 18:24:52 -0700

    changed AudioGram

--- Hearing Test/gen/com/capstone/hearingtest/R.java
+++ Hearing Test/gen/com/capstone/hearingtest/R.java
@@ -22,29 +22,30 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int BTN_play=0x7f09000b;
-        public static final int BTN_submit=0x7f09000c;
-        public static final int ET_frequency=0x7f09000f;
-        public static final int LL_vis=0x7f090008;
-        public static final int PB_test_progress=0x7f09000e;
-        public static final int audiogram=0x7f090015;
-        public static final int btn_create_account=0x7f090007;
-        public static final int btn_getstarted=0x7f090012;
-        public static final int help=0x7f090014;
-        public static final int ll_EQ=0x7f090011;
-        public static final int mySimpleXYPlot=0x7f090000;
-        public static final int seekbar=0x7f090009;
-        public static final int share=0x7f090016;
-        public static final int spinner_age=0x7f090006;
-        public static final int spinner_gender=0x7f090004;
-        public static final int tbtn_play=0x7f090013;
-        public static final int tbtn_record=0x7f090010;
-        public static final int textView1=0x7f090001;
-        public static final int textView2=0x7f090003;
-        public static final int textView3=0x7f090005;
-        public static final int textview=0x7f09000a;
-        public static final int tv_account=0x7f090002;
-        public static final int x=0x7f09000d;
+        public static final int BTN_play=0x7f09000c;
+        public static final int BTN_submit=0x7f09000d;
+        public static final int ET_frequency=0x7f090010;
+        public static final int LL_vis=0x7f090009;
+        public static final int PB_test_progress=0x7f09000f;
+        public static final int audiogram=0x7f090016;
+        public static final int btn_create_account=0x7f090008;
+        public static final int btn_getstarted=0x7f090013;
+        public static final int help=0x7f090015;
+        public static final int ll_EQ=0x7f090012;
+        public static final int mySimpleXYPlot=0x7f090001;
+        public static final int scrollView1=0x7f090000;
+        public static final int seekbar=0x7f09000a;
+        public static final int share=0x7f090017;
+        public static final int spinner_age=0x7f090007;
+        public static final int spinner_gender=0x7f090005;
+        public static final int tbtn_play=0x7f090014;
+        public static final int tbtn_record=0x7f090011;
+        public static final int textView1=0x7f090002;
+        public static final int textView2=0x7f090004;
+        public static final int textView3=0x7f090006;
+        public static final int textview=0x7f09000b;
+        public static final int tv_account=0x7f090003;
+        public static final int x=0x7f09000e;
     }
     public static final class layout {
         public static final int audiogram=0x7f030000;
